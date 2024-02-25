6f5e1845a8802b53d38e940cf7a2c60caf5bcc91
Bence Ujj <ehog@ehog.hu>
2024-02-25 17:02:08 +0100

    Labelling changes

--- src/app/components/investment-chart/investment-chart.component.ts
+++ src/app/components/investment-chart/investment-chart.component.ts
@@ -74,9 +74,6 @@ export class InvestmentChartComponent {
     chart: {
       height: 350,
       type: 'line',
-      zoom: {
-        enabled: false,
-      },
     },
     dataLabels: {
       enabled: false,
@@ -85,14 +82,10 @@ export class InvestmentChartComponent {
       curve: 'straight',
     },
     title: {
-      text: 'Product Trends by Month',
+      text: 'Your investment',
       align: 'left',
     },
     grid: {
-      row: {
-        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-        opacity: 0.5,
-      },
     },
     xaxis: {},
   };
